ab8b2620272a8010f3109f09c7f341416bfb1c76
Juho Haimakka <haimakka@outlook.com>
2025-11-09 19:40:35 +0200

    Minor fix

--- Mod1/Teht5/script.js
+++ Mod1/Teht5/script.js
@@ -1,5 +1,5 @@
 'use strict';
-const num = Number(prompt('Should I calcucalte'));
+const num = Number(prompt('Enter year.'));
 let choice = '';
 if (num % 4 == 0 && num % 100 != 0 || num % 400 == 0) {
     document.querySelector('#target').innerHTML = num + ' is a leap year';    
